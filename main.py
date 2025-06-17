d464afe4889c0dfc2ba92bf7fda350f8f04b87bf
Prince John <125338190+princejohnongit@users.noreply.github.com>
2025-06-17 12:02:09 +0530

    final

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from langchain_anthropic import ChatAnthropic
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
 from langchain.agents import create_tool_calling_agent,AgentExecutor
-from tools import search_tool,wiki_tool
+from tools import search_tool,wiki_tool,save_tool
 
 load_dotenv()
 #API_KEY=
@@ -34,20 +34,21 @@ prompt=ChatPromptTemplate.from_messages(
          ("placeholder","{agent_scratchpad}")
     ]).partial(format_instructions=parser.get_format_instructions())
 
-tools=[search_tool,wiki_tool]
+tools=[search_tool,wiki_tool,save_tool]
 agent=create_tool_calling_agent(
     llm=llm,
     prompt=prompt,
     tools=tools
 )
 query=input("What can i help to reasearch for you? ")
-agent_executor=AgentExecutor(agent=agent,tools=[],verbose=True)
-raw_response=agent_executor.invoke({"query": query})
+raw_response=None
 #print(raw_response)
 try:
+    agent_executor=AgentExecutor(agent=agent,tools=[],verbose=True)
+    raw_response=agent_executor.invoke({"query": query})
     structured_response=parser.parse(raw_response.get('output'))[0]["text"]
     print(structured_response)
 except Exception as e:
-    print("Error Parsing REsponse",e,"Raw_repsonse - ", raw_response)
+    print("Error Parsing REsponse",e,"-------Raw_repsonse----- ", raw_response)
 
 
